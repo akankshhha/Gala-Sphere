0324d77ac9aafbab47bbc3630cbd1c6ca92d7078
akanksha <17aks11@gmail.com>
2024-08-27 12:22:01 +0530

    segregated client and server component for departments

--- app/departments/[departmentId]/page.tsx
+++ app/departments/[departmentId]/page.tsx
@@ -1,6 +1,5 @@
 import { getObjectIDs, getObjectDetails, getDepartments } from '../../api/route';
-import ObjectCard from '../../components/static/ObjectCard';
-import Link from 'next/link';
+import DepartmentObjectsClient from '../[departmentId]/DepartmentObjectsClient'
 
 const ITEMS_PER_PAGE = 12;
 
@@ -22,36 +21,9 @@ const DepartmentPage = async ({ params, searchParams }: { params: { departmentId
     const { departments } = await getDepartments();
     const department = departments.find((dept: { departmentId: number, displayName: string }) => dept.departmentId === Number(departmentId));
 
-    return (
-        <div className='w-11/12 mx-auto p-10'>
-            {/* Breadcrumb Navigation */}
-            <nav className="mb-6">
-                <ul className="flex space-x-2 text-sm text-gray-700">
-                    <li>
-                        <Link href="/departments" className="hover:underline">Departments</Link>
-                    </li>
-                    <li>/</li>
-                    <li className="text-gray-500"> {department.displayName || 'Unknown Department'}</li>
-                </ul>
-            </nav>            <h1 className="text-2xl font-bold mb-4">Objects in Department {departmentId}</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
-                {objectDetails.map((object: any) => (
-                    <Link key={object.objectID} href={`/objects/${object.objectID}`}>
-                        <ObjectCard primaryImage={object.primaryImage} title={object.title} medium={object.medium} artist={object.artistDisplayName} objectBeginDate={object.objectBeginDate} objectEndDate={object.objectEndDate}/>
-                    </Link>
-                ))}
-            </div>
-            <div className="flex justify-between mt-6">
-                <Link href={`?page=${page - 1}`} passHref>
-                    <button disabled={page <= 1} className="bg-red-100 p-2 rounded-md">Previous</button>
-                </Link>
-                <span>Page {page} of {totalPages}</span>
-                <Link href={`?page=${page + 1}`} passHref>
-                    <button disabled={page >= totalPages} className="bg-red-100 p-2 rounded-md">Next</button>
-                </Link>
-            </div>
-        </div>
-    );
+   return(
+        <DepartmentObjectsClient objectDetails = {objectDetails} totalPages = {totalPages} department={department} page = {page} start={start} end={end}/>
+   )
 };
 
 export default DepartmentPage;
